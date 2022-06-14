d4e94c063ce53c8719c90be3f7d61a1d91cc8a0b
Andy <andy@apurata.com>
2022-06-14 18:33:16 -0500

    Javascript Array methods

--- escuela desarrollo web/Javascript/EstructurasDeDatos/Array.js
+++ escuela desarrollo web/Javascript/EstructurasDeDatos/Array.js
@@ -1,24 +1,43 @@
 class MyArray {
-constructor() {
-    this.length = 0;
-    this.data = {};
-}
+	constructor() {
+		this.length = 0;
+		this.data = {};
+	}
 
-get(index) {
-    return this.data[index];
-}
+	get(index) {
+		return this.data[index];
+	}
 
-push(item) {
-    this.data[this.length] = item;
-    this.length++;
-    return this.data;
-}
+	push(item) {
+		this.data[this.length] = item;
+		this.length++;
+		return this.data;
+	}
+	pop() {
+		const lastItem = this.data[this.length-1];
+		delete this.data[this.length-1];
+		this.length--;
+		return lastItem;
+	}
+	delete(index) {
+			const item = this.data[index];
+			this.shiftIndex(index);
+			return item;
+	}
+	shiftIndex(index) {
+		for(let i = index;i<this.length-1;i++){
+			this.data[i] = this.data[i+1];
+		}
+		this.pop();
+		return this.data;
+	}
 }
 
 const myArray = new MyArray();
 myArray.push('Diego');
 myArray.push('Karen');
 myArray.push('Oscar');
+// console.log(myArray.pop());
+console.log(myArray.push('Andy'));
+console.log(myArray.delete(1));
 console.log(myArray);
-console.log(myArray.get(1));
-console.log(myArray.length);
