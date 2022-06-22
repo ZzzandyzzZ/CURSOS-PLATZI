912a50aad72dc5f3965aa0f064c9f416e908ca4b
ZzzandyzzZ <andy_gean@hotmail.com>
2022-06-22 18:10:44 -0500

    Fix typo

--- escuela desarrollo web/Javascript/Nodejs/fetch.js
+++ escuela desarrollo web/Javascript/Nodejs/fetch.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const get = async () => {
     try {
       const initialData = await fetch('https://rickandmortyapi.com/api');
-      const initialDataJson = await algoMas.json();
+      const initialDataJson = await initialData.json();
       console.log(initialDataJson);
     } catch (error) {
       console.error(error);
