e4e05682022e399bd893acd21d29369475bd2689
ZzzandyzzZ <andy_gean@hotmail.com>
2022-06-29 10:50:53 -0500

    Using transforms in file with nodejs

--- escuela desarrollo web/Javascript/Nodejs/main.js
+++ escuela desarrollo web/Javascript/Nodejs/main.js
@@ -1,15 +1,1 @@
 const log = console.log;
-
-import fs from 'fs';
-
-let readableStream = fs.createReadStream('fileTest.txt');
-let data = '';
-readableStream.setEncoding('UTF8');
-readableStream.on('data', (chunk) => {
-  // log(chunk); // Buffer
-  log(chunk);
-  data += chunk;
-});
-readableStream.on('end', () => {
-  log(data);
-});
--- escuela desarrollo web/Javascript/Nodejs/stream.js
+++ escuela desarrollo web/Javascript/Nodejs/stream.js
@@ -4,12 +4,25 @@ import fs from 'fs';
 
 let readableStream = fs.createReadStream('fileTest.txt');
 let data = '';
-readableStream.setEncoding('UTF8');
+readableStream.setEncoding('UTF8'); // Lo decodifica en este formato, ya no es necesario usar toString()
 readableStream.on('data', (chunk) => {
-  // log(chunk); // Buffer
-  log(chunk);
+  log(chunk); // Buffer
   data += chunk;
 });
 readableStream.on('end', () => {
   log(data);
 });
+
+// process.stdout ya es un buffer de escritura
+process.stdout.write('hello ');
+process.stdout.write('world');
+
+const upperCaseTransform = new stream.Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().toUpperCase()); // enviar la data
+  }
+});
+
+const readableStream2 = fs.createReadStream('fileTest.txt');
+
+readableStream2.pipe(upperCaseTransform).pipe(process.stdout)
