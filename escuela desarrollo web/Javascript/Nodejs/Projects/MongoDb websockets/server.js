f7526f5dfd757cfdf2eba3bbf9b6a88ebabeaefd
ZzzandyzzZ <andy_gean@hotmail.com>
2022-06-29 20:46:12 -0500

    Using router and body parser in express

--- escuela desarrollo web/Javascript/Nodejs/Projects/MongoDb websockets/server.js
+++ escuela desarrollo web/Javascript/Nodejs/Projects/MongoDb websockets/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
 
+const log = console.log;
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) =>{
   res.send('Method GET');
+});
+
+app.post('/', (req, res) =>{
+  log(req.body);
+  log(req.query);
+  res.send({body: req.body, query: req.query});
 });
 
 app.listen(port, () => {
