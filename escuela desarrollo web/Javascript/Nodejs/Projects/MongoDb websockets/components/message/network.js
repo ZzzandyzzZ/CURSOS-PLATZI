328f93d08032c441302ac0b00de485fa83f54ed7
ZzzandyzzZ <andy_gean@hotmail.com>
2022-07-01 13:54:17 -0500

    Fix error response

--- escuela desarrollo web/Javascript/Nodejs/Projects/MongoDb websockets/components/message/network.js
+++ escuela desarrollo web/Javascript/Nodejs/Projects/MongoDb websockets/components/message/network.js
@@ -1,13 +1,14 @@
-import express from 'express';
+import {Router} from 'express';
 
 import {success, error} from '../../network/response.js';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/', (req, res) =>{
   res.header({'custom': 'value1'});
   if (req.query.ok === 'false') {
     error(req, res, 'Intensional error', 500, 'it was on purpose');
+    return;
   }
   success(req, res, 'Hello grom get', 200);
 });
