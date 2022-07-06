c927077464682fcccd1a3a96bc8a745dca520427
ZzzandyzzZ <andy_gean@hotmail.com>
2022-07-06 17:21:41 -0500

    Using mongoose to connect with DB

--- escuela desarrollo web/Javascript/Nodejs/Projects/MongoDb websockets/src/components/message/store.ts
+++ escuela desarrollo web/Javascript/Nodejs/Projects/MongoDb websockets/src/components/message/store.ts
@@ -1,11 +1,24 @@
-import { Message } from '../../types';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const list:Array<Message> = [];
+import { Message } from '../../types';
+import MsjModel from './model';
+
+dotenv.config();
+
+const URI = process.env.MONGO_URI || '';
+mongoose.connect(URI);
+const db = mongoose.connection;
+db.on('error', (e) => console.error('error', e));
+db.once('open', () => console.log('Mongoose is connected'));
+
 const addMessage = (message:Message) => {
-  list.push(message);
+  const newMessage = new MsjModel(message);
+  newMessage.save();
 };
 
-const listMessages = ():Array<Message> => {
-  return list;
+const listMessages = async ():Promise<Array<Message>> => {
+  const data:Array<Message> = await MsjModel.find({});
+  return data;
 };
 export { addMessage, listMessages };
