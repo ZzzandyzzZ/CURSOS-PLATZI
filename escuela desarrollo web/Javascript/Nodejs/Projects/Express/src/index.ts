cc868e7359ff799f20279e621f2e9b8d4212db01
ZzzandyzzZ <andy_gean@hotmail.com>
2022-07-07 21:36:32 -0500

    Use morgan to log http request

--- escuela desarrollo web/Javascript/Nodejs/Projects/Express/src/index.ts
+++ escuela desarrollo web/Javascript/Nodejs/Projects/Express/src/index.ts
@@ -1,9 +1,14 @@
 import express, { Application } from 'express';
+import morgan from 'morgan';
 
 import config from './config';
 
 const app:Application = express();
 const { port, host } = config;
+
+console.clear();
+
+app.use(morgan('dev'));
 
 app.use('/', (req, res) => {
   res.send('Hello world');
